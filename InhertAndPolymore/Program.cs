b6dcc83cfd8bdf0e9ef8d2078ee7c5cd53100d9b
mohammad ibrahim <mohammad960@revature.net>
2022-08-02 03:33:03 -0500

    the hacker rank commit

---
+++ InhertAndPolymore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InhertAndPolymore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -6,7 +6,11 @@ namespace consoleApp
     {
         static void Main(string[] args)
         {
+           Console.WriteLine("Welcome to .net!");
             Console.WriteLine("Welcome to .net!");
+           Console.WriteLine("Welcome !");
+            Console.WriteLine("to !");
+            Console.WriteLine(".net!");
         }
     }
 }
